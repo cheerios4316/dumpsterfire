824896bedeb875e86b22bcecc775c6f7c191e588
egidio.pignataro <egidio.pignataro@gruppomol.it>
2025-03-17 09:42:06 +0100

    work in progress modifications, back to my day job :(

---
+++ src/TestComponent/script.TestComponent.ts
@@ -0,0 +1,6 @@
+import {Component} from "../../repos/dumpsterfire-components/src/js/Application";
+export class TestComponent extends Component {
+    bindEvents() {
+        console.log('test');
+    }
+}
